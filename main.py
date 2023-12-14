219146a9a56b4c446816e46165ce13166af21f40
Soulbolt <soulbolt@gmail.com>
2023-12-14 11:38:31 -0700

    added if statement to check for users bet and a while loop based on that bet.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import random
 from turtle import Turtle, Screen
 
+is_race_on = False
 screen = Screen()
 screen.setup(width=500, height=400)
 user_bet = screen.textinput(title="Make your bet.", prompt="Which turtle will win the race? Enter a color: ")
@@ -8,15 +10,18 @@ colors = ["red", "orange", "yellow", "green", "blue", "purple"]
 y_positions = [-70, -40, -10, 20, 50, 80]
 
 for turtle_index in range(0, 6):
-    don = Turtle(shape="turtle")
-    don.color(colors[turtle_index])
-    don.penup()
-    don.goto(x=-230, y=y_positions[turtle_index])
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.color(colors[turtle_index])
+    new_turtle.penup()
+    new_turtle.goto(x=-230, y=y_positions[turtle_index])
 
+if user_bet:
+    is_race_on = True
 
+while is_race_on:
 
-
-
-
+    random_distance = random.randint(0, 10)
+    x_position = 250
+    y_position = 0
 
 screen.exitonclick()
